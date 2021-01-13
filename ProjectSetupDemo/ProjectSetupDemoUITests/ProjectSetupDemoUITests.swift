fabcd79024bbed83fb164b8e57c1ee2fc519dd7f
Dominic Mülhaupt <dominic.muelhaupt@fhnw.ch>
2021-01-13 20:42:16 +0100

    Configuration to run fastlane snapshot

--- ProjectSetupDemo/ProjectSetupDemoUITests/ProjectSetupDemoUITests.swift
+++ ProjectSetupDemo/ProjectSetupDemoUITests/ProjectSetupDemoUITests.swift
@@ -13,7 +13,7 @@ class ProjectSetupDemoUITests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -31,14 +31,5 @@ class ProjectSetupDemoUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         snapshot("Hello World View")
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
     }
 }
