70315a3aff642294d675eea215480468fbf615e8
Dominic Mülhaupt <dominic.muelhaupt@fhnw.ch>
2021-01-13 19:05:13 +0100

    Setup fastlane Snapshots

--- ProjectSetupDemo/ProjectSetupDemoUITests/ProjectSetupDemoUITests.swift
+++ ProjectSetupDemo/ProjectSetupDemoUITests/ProjectSetupDemoUITests.swift
@@ -25,10 +25,12 @@ class ProjectSetupDemoUITests: XCTestCase {
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        snapshot("Hello World View")
     }
 
     func testLaunchPerformance() throws {
