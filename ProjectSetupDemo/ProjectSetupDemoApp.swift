c6525f428e05250c882780fedb56faa4f45a302e
Dominic Mülhaupt <dominic.muelhaupt@fhnw.ch>
2021-01-13 12:02:30 +0100

    Initial Commit

---
+++ ProjectSetupDemo/ProjectSetupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectSetupDemoApp.swift
+//  ProjectSetupDemo
+//
+//  Created by FHNW on 13.01.21.
+//
+
+import SwiftUI
+
+@main
+struct ProjectSetupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
